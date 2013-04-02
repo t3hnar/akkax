450bc702a238609e888557a6573c411d5d5f936c
Yaroslav Klymko <t3hnar@gmail.com>
2013-04-02 11:23:19 +0200

    fix IgnoreIfBusyExample

--- src/main/scala/ua/t3hnar/akkax/examples/IgnoreIfBusyExample.scala
+++ src/main/scala/ua/t3hnar/akkax/examples/IgnoreIfBusyExample.scala
@@ -1,13 +1,13 @@
 package ua.t3hnar.akkax
 package examples
 
-import akka.actor.Actor
+import akka.actor.{ActorLogging, Actor}
 import ua.t3hnar.akkax.IgnoreIfBusy
 
 /**
  * @author Yaroslav Klymko
  */
-class IgnoreIfBusyExample extends Actor with IgnoreIfBusy {
+class IgnoreIfBusyExample extends Actor with ActorLogging with IgnoreIfBusy {
   protected def receive = receiveRun
   def run() {
     // heavy call
